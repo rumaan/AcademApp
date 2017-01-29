9ab99c3e11b80d6792f5378ba192e20bc90a26d7
Rumaan <rumaankalander@gmail.com>
2017-01-29 09:17:23 +0530

    Added Splash Page

---
+++ app/src/main/java/com/rumaan/academapp/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.rumaan.academapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/rumaan/academapp/SplashPageActivity.java
@@ -0,0 +1,13 @@
+package com.rumaan.academapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
