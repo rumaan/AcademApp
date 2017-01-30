4aa043721f85c443891b771c10ff13c2fac8bc98
Rumaan <rumaankalander@gmail.com>
2017-01-30 19:42:24 +0530

    Removed Toolbar going for my custom implementation

--- app/src/main/java/com/rumaan/academapp/HomeActivity.java
+++ app/src/main/java/com/rumaan/academapp/HomeActivity.java
@@ -10,4 +10,5 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
 }
--- app/src/main/java/com/rumaan/academapp/SplashPageActivity.java
+++ app/src/main/java/com/rumaan/academapp/SplashPageActivity.java
@@ -1,5 +1,6 @@
 package com.rumaan.academapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,14 @@ public class SplashPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        /* after loading goto next activity */
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startActivity(new Intent(SplashPageActivity.this, HomeActivity.class));
     }
 }
