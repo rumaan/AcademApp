294c12e18ec463397dba24781be2054e07bf63c6
Rumaan <rumaankalander@gmail.com>
2017-04-30 21:24:53 +0530

    Added few things to UserDetails activity

--- app/src/main/java/com/rumaan/academapp/activities/UserDetailsActivity.java
+++ app/src/main/java/com/rumaan/academapp/activities/UserDetailsActivity.java
@@ -1,32 +1,26 @@
 package com.rumaan.academapp.activities;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.transition.AutoTransition;
-import android.transition.Explode;
-import android.transition.Fade;
-import android.transition.Scene;
-import android.transition.Slide;
-import android.transition.TransitionInflater;
-import android.transition.TransitionManager;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.rumaan.academapp.R;
 
+import java.util.regex.Pattern;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
@@ -46,12 +40,30 @@ public class UserDetailsActivity extends AppCompatActivity {
     @BindView(R.id.college_name)
     TextInputLayout collegeNameInput;
 
+    @BindView(R.id.course_spinner)
+    Spinner courseSpinner;
+    @BindView(R.id.year_spinner)
+    Spinner yearSpinner;
+
     @OnClick(R.id.btn_next)
     void onClick() {
-        TransitionManager.go(Scene.getSceneForLayout(rootView, R.layout.activity_home, this)
-                , TransitionInflater.from(this).inflateTransition(R.transition.into_main_screen));
-
-        startActivity(new Intent(this, MainActivity.class));
+        // Validate the fields before going to next activity
+        Editable collegeName = collegeNameInput.getEditText().getText();
+        Editable usn = usnTextInput.getEditText().getText();
+        if (TextUtils.isEmpty(collegeName)) {
+            collegeNameInput.setErrorEnabled(true);
+            collegeNameInput.setError(getString(R.string.valid_college_name_error));
+            return;
+        }
+        if (TextUtils.isEmpty(usn)) {
+            usnTextInput.setErrorEnabled(true);
+            usnTextInput.setError(getString(R.string.valid_usn_error));
+            return;
+        }
+        collegeNameInput.setErrorEnabled(false);
+        usnTextInput.setErrorEnabled(false);
+        Toast.makeText(this, "All good!", Toast.LENGTH_SHORT).show();
+        //    startActivity(new Intent(this, MainActivity.class));
     }
 
 
@@ -63,28 +75,88 @@ public class UserDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+
         setContentView(R.layout.activity_user_details);
+
+
         ButterKnife.bind(this);
+
+
+        ArrayAdapter coursesList = ArrayAdapter.createFromResource(this, R.array.courses_list, R.layout.spinner_item);
+        ArrayAdapter yearList = ArrayAdapter.createFromResource(this, R.array.year_list, R.layout.spinner_item);
+
+        courseSpinner.setAdapter(coursesList);
+        yearSpinner.setAdapter(yearList);
+
+        // add text watchers to input fields
+        collegeNameInput.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (isCollegeNameValid(s)) {
+                    collegeNameInput.setError(getString(R.string.valid_college_name_error));
+                    collegeNameInput.setErrorEnabled(true);
+                } else {
+                    collegeNameInput.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+
+        usnTextInput.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (isUsnValid(s)) {
+                    usnTextInput.setErrorEnabled(true);
+                    usnTextInput.setError(getString(R.string.valid_usn_error));
+                } else {
+                    usnTextInput.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    boolean isCollegeNameValid(CharSequence s) {
+        // TODO: Check for more invalid characters
+        return !Pattern.matches("^(a-z|A-Z|0-9)*[^#$%^&*()/+_-]*$", s.toString());
+    }
+
+    boolean isUsnValid(CharSequence s) {
+        // TODO: create regExp particular to USN
+        return !Pattern.matches("^(a-z|A-Z|0-9)*[^#$%^&*()' /+_-]*$", s.toString());
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
-    /* Inner class which gets triggered on text change in the text input fields */
-    class MyTextWatcher implements TextWatcher {
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            // do something before text changes
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            // do something when text is changing
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            // do something after text has changed
-        }
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
